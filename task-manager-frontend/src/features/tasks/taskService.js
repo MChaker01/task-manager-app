f7448739ec28accc62b290e6f3fff81acbb9a21b
Mohamed Chaker OUAADDI <med18chaker@gmail.com>
2025-09-30 19:38:31 +0100

    feat: add taskService with CRUD operations

---
+++ task-manager-frontend/src/features/tasks/taskService.js
@@ -0,0 +1,58 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:3000/api/tasks";
+
+const getAuthConfig = () => {
+  const user = JSON.parse(localStorage.getItem("user"));
+
+  if (user && user.token) {
+    return {
+      headers: {
+        Authorization: `Bearer ${user.token}`,
+      },
+    };
+  }
+
+  return {};
+};
+
+// Créer une tâche :
+const createTask = async (taskData) => {
+  const response = await axios.post(API_URL, taskData, getAuthConfig());
+
+  return response.data;
+};
+
+// Récupérer toutes les tâches :
+const getTasks = async () => {
+  const response = await axios.get(API_URL, getAuthConfig());
+
+  return response.data;
+};
+
+// Supprimer une tâche :
+const deleteTask = async (taskId) => {
+  const response = await axios.delete(API_URL + `/${taskId}`, getAuthConfig());
+
+  return response.data;
+};
+
+// modifier une tâche :
+const updateTask = async (taskId, taskData) => {
+  const response = await axios.put(
+    API_URL + `/${taskId}`,
+    taskData,
+    getAuthConfig()
+  );
+
+  return response.data;
+};
+
+const taskService = {
+  createTask,
+  getTasks,
+  deleteTask,
+  updateTask,
+};
+
+export default taskService;
